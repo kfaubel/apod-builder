a476f0cad159fb57bcf044923d32e9fe48095f01
Ken Faubel <ken@faubel.org>
2021-09-18 11:24:22 -0400

    Title color tweak

--- src/ApodImage.ts
+++ src/ApodImage.ts
@@ -28,18 +28,29 @@ export class ApodImage {
 
     // This optimized fillRect was derived from the pureimage source code: https://github.com/joshmarinacci/node-pureimage/tree/master/src
     // To fill a 1920x1080 image on a core i5, this saves about 1.5 seconds
+    // img        - image - it has 3 properties height, width and data
     // x, y       - position of the rect
     // w, h       - size of the rect
-    // iw         - width of the image being written into, needed to calculate index into the buffer
-    // r, g, b, a - values to draw
-    private myFillRect(image: Buffer, x: number, y: number, w: number, h: number, iw: number, r: number, g: number, b: number, a: number) {
+    // rgb        - must be a string in this form "#112233"
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    private myFillRect(img: any, x: number, y: number, w: number, h: number, rgb: string) {
+        const colorValue = parseInt(rgb.substring(1), 16);
+
+        // the shift operator forces js to perform the internal ToUint32 (see ecmascript spec 9.6)
+        //colorValue = colorValue >>> 0;
+        const r = (colorValue >>> 16) & 0xFF;
+        const g = (colorValue >>> 8)  & 0xFF;  
+        const b = (colorValue)        & 0xFF;
+        const a = 0xFF;
+
         for(let i = y; i < y + h; i++) {                
             for(let j = x; j < x + w; j++) {   
-                const index = (i * iw + j) * 4;     
-                image[index + 0] = r; 
-                image[index + 1] = g; 
-                image[index + 2] = b; 
-                image[index + 3] = a; 
+                const index = (i * img.width + j) * 4;   
+                
+                img.data[index + 0] = r;
+                img.data[index + 1] = g;     
+                img.data[index + 2] = b;     
+                img.data[index + 3] = a; 
             }
         }
     }
@@ -55,10 +66,10 @@ export class ApodImage {
         const titleOffset              = 25;
 
         const backgroundColor          = "#000000";
-        const titleColor               = "rgb(42,  32,  200)";  //"rgb(40,  200,  80)"; 
+        const titleColor               = "rgb(10,  210,  250)";  //"rgb(40,  200,  80)"; 
 
         const largeFont                = "72px 'OpenSans-Bold'";     // Title
-        const mediumFont               = "48px 'OpenSans-Regular";   // Other text
+        const mediumFont               = "60px 'OpenSans-Regular";   // Other text
         const smallFont                = "40px 'OpenSans-Regular'";  // Note at the bottom
         const extraSmallFont           = "30px 'OpenSans-Regular'";  // Note at the bottom
 
@@ -77,14 +88,14 @@ export class ApodImage {
         // Fill the background
         ctx.fillStyle = backgroundColor;
         //ctx.fillRect(0, 0, imageWidth, imageHeight);
-        this.myFillRect(img.data, 0, 0, imageWidth, imageHeight, imageWidth, 0x00, 0x00, 0x20, 0);
+        this.myFillRect(img, 0, 0, imageWidth, imageHeight, backgroundColor);
 
         try {
             let picture: jpeg.BufferRet | null = null;
 
             if (apodJson.hdurl !== undefined) {
                 const pictureUrl = (apodJson.hdurl as string);
-                this.logger.verbose(`ApodImage: hdurl: ${pictureUrl}`);
+                // this.logger.verbose(`ApodImage: hdurl: ${pictureUrl}`);
                 
                 // first try to download a jpg
                 try {
